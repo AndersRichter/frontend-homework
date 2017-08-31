932d87e106535a6c15ecc53662369bee59651d3e
AndersRichter <andreysav96@rambler.ru>
2017-09-01 00:02:51 +0300

    Update chess.js
    все исправил, кроме замены isNaN() на Number.isNaN(). Вторая функция почему то некорректно работает в этом случае.

--- source/chess.js
+++ source/chess.js
@@ -1,15 +1,15 @@
 'use strict';
 
 const chess = function (number) {
-	if (number <= 1 || isNaN(number)) return null;
-	else {
-		var chessDesk = '';
-		for (var i = 0; i < number; i++) {
-	      		for (var j = 0; j < number; j++) {
-	      			chessDesk += ((i + j)%2 == 0) ? '*' : ' ';
-	      		}
-	      		chessDesk += '\n';
-		}
-	    	return chessDesk;
+	if (isNaN(number) || number <= 1) {
+		return null;
 	}
+	let chessDesk = '';
+	for (let i = 0; i < number; i++) {
+	    for (let j = 0; j < number; j++) {
+	    	chessDesk += ((i + j) % 2 === 0) ? '*' : ' ';
+	    }
+	    chessDesk += '\n';
+	}
+	return chessDesk;  		
 };
