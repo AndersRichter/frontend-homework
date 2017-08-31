9be9672f8f0a50800c2c0848f25f1d5df9ced3f1
AndersRichter <andreysav96@rambler.ru>
2017-08-31 05:24:09 +0300

    Update chess.js

--- tests/chess.js
+++ tests/chess.js
@@ -1,6 +1,16 @@
 'use strict';
 
 QUnit.module('Тестируем функцию chess', function () {
+	QUnit.test('Нельзя ввести не число', function (assert) {
+		assert.strictEqual(chess('Hello'), null);
+		assert.strictEqual(chess('|(o_o)|'), null);
+	});
+
+	QUnit.test('Шахматной доски 0 на 0 не бывает', function (assert) {
+		assert.strictEqual(chess(0), null);
+		assert.strictEqual(chess('0'), null);
+	});
+
 	QUnit.test('Шахматной доски 1 на 1 не бывает', function (assert) {
 		assert.strictEqual(chess(1), null);
 		assert.strictEqual(chess('1'), null);
@@ -35,6 +45,28 @@ QUnit.module('Тестируем функцию chess', function () {
 			' * * * *\n' ;
 		assert.strictEqual(chess(8), expected);
 		assert.strictEqual(chess('8'), expected);
+	});
+
+	QUnit.test('Шахматная доска 16 на 16', function (assert) {
+		const expected =
+			'* * * * * * * * \n' +
+			' * * * * * * * *\n' +
+			'* * * * * * * * \n' +
+			' * * * * * * * *\n' +
+			'* * * * * * * * \n' +
+			' * * * * * * * *\n' +
+			'* * * * * * * * \n' +
+			' * * * * * * * *\n' +
+			'* * * * * * * * \n' +
+			' * * * * * * * *\n' +
+			'* * * * * * * * \n' +
+			' * * * * * * * *\n' +
+			'* * * * * * * * \n' +
+			' * * * * * * * *\n' +
+			'* * * * * * * * \n' +
+			' * * * * * * * *\n' ;
+		assert.strictEqual(chess(16), expected);
+		assert.strictEqual(chess('16'), expected);
 	});
 
 });
